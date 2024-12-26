4006fdf7f6c45c28bb779eed879438450c7a3419
Lucher <Lucdudh1~>
2024-12-26 19:20:18 +0300

    dsfsdf

---
+++ 1.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+let num1 = prompt("Введите первое число :");
+let num2 = prompt("Введите второе число :");
+let sum = 0;
+
+for ( let i = num; i<= num2; i++){
+    summa += 1;
+}
+
+console.log(`сумма равна ${summa}`)
